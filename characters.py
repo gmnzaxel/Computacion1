294ec9ac13d070348c609dfb6a9601a6f2570085
Axel <screaxgameplays@gmail.com>
2024-09-02 18:24:21 -0300

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,23 +1,23 @@
 class Character:
     def __init__(self, name, health, strength, defense, level=1, experience=0):
-        self.name = name
-        self.health = health
-        self.strength = strength
-        self.defense = defense
-        self.level = level
-        self.experience = experience
+        self.__name = name
+        self.__health = health
+        self.__strength = strength
+        self.__defense = defense
+        self.__level = level
+        self.__experience = experience
 
    #! Funcion atacar
     def attack(self, enemy):
-        damage = max(self.strength - enemy.defense, 0)
-        enemy.take_damage(damage)
-        print(f"{self.name} atacó a {enemy.name} causando {damage} puntos de daño.")
+        damage = max(self.__strength - enemy.__defense, 0)
+        enemy.__take_damage(damage)
+        print(f"{self.__name} atacó a {enemy.__name} causando {damage} puntos de daño.")
 
     #! Funcion recibir daño, considerando defensa
     def take_damage(self, damage):
-        actual_damage = max(damage - self.defense, 0)
-        self.health -= actual_damage
-        print(f"{self.name}, recibiste {actual_damage} puntos de daño. Salud restante: {self.health}")
+        actual_damage = max(damage - self.__defense, 0)
+        self.__health -= actual_damage
+        print(f"{self.__name}, recibiste {actual_damage} puntos de daño. Salud restante: {self.__health}")
 
     #! Funcion ganar experiencia
     def gain_experience(self, points):
