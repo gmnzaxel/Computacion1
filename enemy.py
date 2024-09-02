5b698adee3092457d17795c54e98a73a7669d5c6
Axel <screaxgameplays@gmail.com>
2024-09-02 18:32:08 -0300

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,27 +1,27 @@
 class Enemy:
     def __init__(self, name, health, shield, damage):
-        self.name = name
-        self.health = health
-        self.shield = shield
-        self.damage = damage
+        self.__name = name
+        self.__health = health
+        self.__shield = shield
+        self.__damage = damage
 
     def take_damage(self, amount):
         #Reduce el escudo y luego la vida en función del daño recibido.
-        if self.shield > 0:
+        if self.__shield > 0:
             remaining_damage = amount - self.shield
-            self.shield = max(self.shield - amount, 0)
+            self.__shield = max(self.__shield - amount, 0)
             if remaining_damage > 0:
-                self.health = max(self.health - remaining_damage, 0)
+                self.__health = max(self.__health - remaining_damage, 0)
         else:
-            self.health = max(self.health - amount, 0)
+            self.__health = max(self.__health - amount, 0)
 
     def attack(self):
         #Devuelve el daño que puede infligir el enemigo.
-        return self.damage
+        return self.__damage
 
     def is_alive(self):
         #Verifica si el enemigo sigue vivo.
-        return self.health > 0
+        return self.__health > 0
 
     def __str__(self):
-        return (f"{self.name} - Vida: {self.health}, Escudo: {self.shield}, Daño: {self.damage}")
+        return (f"{self.__name} - Vida: {self.__health}, Escudo: {self.__shield}, Daño: {self.__damage}")
